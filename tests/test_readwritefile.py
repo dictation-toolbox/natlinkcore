966e13f9a5c363d7a143a6b86a6d9d5b95e2bd1b
Doug Ransom <doug.ransom@gmail.com>
2022-08-22 12:12:17 -0700

    restored main version of readwritefile

--- tests/test_readwritefile.py
+++ tests/test_readwritefile.py
@@ -2,50 +2,26 @@
 #pylint:disable= C0114, C0116, R1732
 import os
 import configparser
-from typing import List
 import pytest
 from natlinkcore.readwritefile import ReadWriteFile
-from shutil import copytree,rmtree
+
 thisFile = __file__
 thisDir, Filename = os.path.split(thisFile)
+testDir = os.path.join(thisDir, 'readwritefiletest')
 
 
+def setup_module(module):
+    pass
 
-@pytest.fixture(scope="module")
-def testDir(tmpdir_factory):
-    test_dir = tmpdir_factory.mktemp("readwritefiletest")
+def teardown_module(module):
+    for F in os.listdir(testDir):
+        if F.startswith('output-'):
+            F_path = os.path.join(testDir, F)
+            os.remove(F_path)
 
-    #pytest will only retain 3 temp directories, so not a problem to leave them
-    print(f"\n{__file__} temp folder is : {test_dir}]\n")
-
-testDir = os.path.join(thisDir, 'mock_readwritefile')
-
-
-def sample_rwfile(sample_name: str) -> List:
-    """
-    give the full path of a test sample file and the corresponding outputfile
-    """
-    inp_path = os.path.join(testDir, sample_name)
-    out_path = testDir
-    return [inp_path, out_path]
-
-#easier than using the decorator syntax
-def make_sample_paths(sample_filename):
-    return pytest.fixture(lambda : sample_rwfile(sample_filename))
-
-    ini_path = make_sample_paths('natlinkinisample.ini')
-
-    #copy the sample files we need for input
-    sampleDir =   os.path.join(thisDir, 'readwritefiletest')
-    copytree(sampleDir,test_dir,dirs_exist_ok=True)
-
-    yield test_dir
-    #in theory can clean up resources here
- 
-
-def test_only_write_file(testDir):
+def test_only_write_file():
     join, isfile = os.path.join, os.path.isfile
-    newFile = join(outputDir, 'output-newfile.txt')
+    newFile = join(testDir, 'output-newfile.txt')
     if isfile(newFile):
         os.unlink(newFile)
     rwfile = ReadWriteFile()
@@ -60,8 +36,7 @@ def test_only_write_file(testDir):
     assert rwfile.bom == ''
     assert text == ''
     
-<<<<<<< HEAD
-def test_accented_characters_write_file(testDir):
+def test_accented_characters_write_file():
     join, isfile = os.path.join, os.path.isfile
     newFile = join(testDir, 'output-accented.txt')
     if isfile(newFile):
@@ -78,86 +53,10 @@ def test_accented_characters_write_file(testDir):
     testTextBinary = open(newFile, 'rb').read()
     assert testTextBinary == b'caf&#233;'
     assert len(testTextBinary) == 9
-=======
-# def test_accented_characters_write_file():
-#     join, isfile = os.path.join, os.path.isfile
-#     newFile = join(testDir, 'output-accented.txt')
-#     if isfile(newFile):
-#         os.unlink(newFile)
-#     text = 'caf\xe9'
-#     rwfile = ReadWriteFile(encodings=['ascii'])  # optional encoding
-#     # this is with default errors='xmlcharrefreplace':
-#     rwfile.writeAnything(newFile, text)
-#     testTextBinary = open(newFile, 'rb').read()
-#     wanted = b'caf&#233;'
-#     assert testTextBinary == wanted
-#     # same, default is 'xmlcharrefreplace':
-#     rwfile.writeAnything(newFile, text, errors='xmlcharrefreplace')
-#     testTextBinary = open(newFile, 'rb').read()
-#     assert testTextBinary == b'caf&#233;'
-#     assert len(testTextBinary) == 9
-# 
-#     text_back = rwfile.readAnything(newFile)
-#     assert text_back == 'caf&#233;'
-#     
-#     rwfile.writeAnything(newFile, text, errors='replace')
-#     testTextBinary = open(newFile, 'rb').read()
-#     assert testTextBinary == b'caf?'
-#     assert len(testTextBinary) == 4
-#     rwfile.writeAnything(newFile, text, errors='ignore')
-#     testTextBinary = open(newFile, 'rb').read()
-#     assert testTextBinary == b'caf'
-#     assert len(testTextBinary) == 3
-#     
-#     rwfile_utf = ReadWriteFile(encodings=['utf-8'])
-#     text = 'Caf\xe9'
-#     rwfile_utf.writeAnything(newFile, text)
-#     text_back = rwfile_utf.readAnything(newFile)
-#     assert text == text_back
-# 
-# def test_other_encodings_write_file():
-#     join = os.path.join
-#     oldFile = join(testDir, 'latin1 accented.txt')
-#     rwfile = ReadWriteFile(encodings=['latin1'])  # optional encoding
-#     text = rwfile.readAnything(oldFile)
-#     assert text == 'latin1 café'
-#     
-#     
-#     
-# 
-# 
-# def test_latin1_cp1252_write_file():
-#     join = os.path.join
-#     _newFile = join(testDir, 'latin1.txt')
-#     _newFile = join(testDir, 'cp1252.txt')
-#     # TODO (QH) to be done, these encodings do not take all characters,
-#     # and need special attention.
-#     # (as long as the "fallback" is utf-8, all write files should go well!)
->>>>>>> be8fa9ac9d587964e20b987251d5a83ca1165286
 
-def test_read_config_file(ini_path):
-    # 
-    # listdir, join, splitext = os.listdir, os.path.join, os.path.splitext
-    # for F in listdir(testDir):
-    #     if F.endswith('.ini'):
-    #         if F == 'acoustics.ini':
-    #             F_path = join(testDir, F)
-    #             rwfile = ReadWriteFile()
-    #             config_text = rwfile.readAnything(F_path)
-    #             Config = configparser.ConfigParser()
-    #             Config.read_string(config_text)
-    #             assert Config.get('Acoustics', '2 2') == '2_2' 
-    #             continue
-    in_path, out_path = ini_path
-    rwfile = ReadWriteFile()
-    config_text = rwfile.readAnything(in_path)
-    Config = configparser.ConfigParser()
-    Config.read_string(config_text)
-    assert Config.get('settings', 'log_level') == 'DEBUG'
-    Config.set('settings', 'log_level', 'INFO')
-    assert Config.get('settings', 'log_level') == 'INFO'
+    text_back = rwfile.readAnything(newFile)
+    assert text_back == 'caf&#233;'
     
-<<<<<<< HEAD
     rwfile.writeAnything(newFile, text, errors='replace')
     testTextBinary = open(newFile, 'rb').read()
     assert testTextBinary == b'caf?'
@@ -173,7 +72,7 @@ def test_read_config_file(ini_path):
     text_back = rwfile_utf.readAnything(newFile)
     assert text == text_back
 
-def test_other_encodings_write_file(testDir):
+def test_other_encodings_write_file():
     join = os.path.join
     oldFile = join(testDir, 'latin1 accented.txt')
     rwfile = ReadWriteFile(encodings=['latin1'])  # optional encoding
@@ -184,7 +83,7 @@ def test_other_encodings_write_file(testDir):
     
 
 
-def test_latin1_cp1252_write_file(testDir):
+def test_latin1_cp1252_write_file():
     join = os.path.join
     _newFile = join(testDir, 'latin1.txt')
     _newFile = join(testDir, 'cp1252.txt')
@@ -192,7 +91,7 @@ def test_latin1_cp1252_write_file(testDir):
     # and need special attention.
     # (as long as the "fallback" is utf-8, all write files should go well!)
 
-def test_read_write_file(testDir):
+def test_read_write_file():
     listdir, join, splitext = os.listdir, os.path.join, os.path.splitext
     for F in listdir(testDir):
         if not F.startswith('output-'):
@@ -206,7 +105,7 @@ def test_read_write_file(testDir):
             rwfile.writeAnything(Fout_path, text)
             assert open(F_path, 'rb').read() == open(Fout_path, 'rb').read()
             
-def test_read_config_file(testDir):
+def test_read_config_file():
     listdir, join, splitext = os.listdir, os.path.join, os.path.splitext
     for F in listdir(testDir):
         if F.endswith('.ini'):
@@ -234,10 +133,7 @@ def test_read_config_file(testDir):
                 Fout = trunk + 'out' + ext
                 Fout_path = join(testDir, Fout)
                 Config.write(open(Fout_path, 'w', encoding=rwfile.encoding))
-=======
-    Config.write(open(out_path, 'w', encoding=rwfile.encoding))
->>>>>>> be8fa9ac9d587964e20b987251d5a83ca1165286
 
 if __name__ == "__main__":
-    pytest.main([f'test_readwritefile.py'])
+    pytest.main(['test_readwritefile.py'])
     
