8cd3e5406deade7313d93201b91502ff2e4d4386
LexiconCode <CasterVoice@protonmail.com>
2022-08-23 10:29:41 -0500

    -Remove natlink UI Column and NatlinkUserDir as Quintijn's feedback suggested it was to confusing
    Natlink using native natlink grammars can still be set by natlinkconfigfunctions.py
    - getAhkExeDir use FolderBrowse

--- src/natlinkcore/configure/nalinkgui.py
+++ src/natlinkcore/configure/nalinkgui.py
@@ -16,8 +16,7 @@ Status = natlinkstatus.NatlinkStatus()
 # Hidden Columns and Project State
 # TODO: if project is enabled, update the project state to enabled.
     # use enabled_packages for vocola, unimacro and dragonfly getDragonflyUserDirectory(), autohotkey Status.getAhkUserDir()?
-natlink, dragonfly, vocola, unimacro, autohotkey = False, False, False, False, False
-
+dragonfly, vocola, unimacro, autohotkey = False, False, False, False
 
 def collapse(layout, key, visible):
     """
@@ -33,10 +32,6 @@ def collapse(layout, key, visible):
 #### Hidden UI Columns ####
 # FIXME: Any text sg.Input/sg.I with enable_events=True will fire the event with every keystroke. 
     # This is a problem when editing the a path in the input field.
-natlink_section = [[sg.Text('Natlink', text_color='black')],
-                    [sg.T('Nalink Loglevel:'),  sg.Combo(default_value=Status.getLogging().title(), values=("Critical",  "Fatal",  "Error", "Warning", "Info" , "Debug"), key='Set_Logging_Natlink', enable_events=True, auto_size_text=True, readonly=True)],
-                    [sg.I(Status.getUserDirectory(), key='Set_UserDir_Natlink',  tooltip=r'The directory where User Natlink grammar files are located (e.g., "~\UserDirectory")', enable_events=True), sg.FolderBrowse(), sg.B("Clear", key='Clear_UserDir_Natlink', enable_events=True)]]
-
 dragonfly_section = [[sg.Text('Dragonfly', text_color='black')],
                      [sg.T('Dragonfly User Directory:', tooltip='The directory to Dragonfly user scripts (UserDirectory can also be used)'), sg.Input(Status.getDragonflyUserDirectory(), key='Set_UserDir_Dragonfly', enable_events=True), sg.FolderBrowse(), sg.B("Clear", key='Clear_UserDir_Dragonfly', enable_events=True)]]
 
@@ -49,16 +44,16 @@ unimacro_section = [[sg.T('Unimacro', text_color='black')],
                     [sg.T('Unimacro User Directory:', tooltip=r'Where the Unimacro user INI files are located, and several other directories (~ or %HOME% allowed)'), sg.I(Status.getUnimacroUserDirectory(), key='Set_UserDir_Unimacro', enable_events=True), sg.FolderBrowse(), sg.B("Clear", key='Clear_UserDir_Unimacro', enable_events=True)]]
 
 autohotkey_section = [[sg.T('Autohotkey', text_color='black')],
-                      [sg.T('Autohotkey Exe:'), sg.I(Status.getAhkExeDir(), key='Set_Exe_Ahk', enable_events=True), sg.FileBrowse(file_types=(('Executable ', '*.exe'),)), sg.B("Clear", key='Clear_Exe_Ahk', enable_events=True)],
+                      [sg.T('Autohotkey EXE Dir:'), sg.I(Status.getAhkExeDir(), key='Set_Exe_Ahk', enable_events=True), sg.FolderBrowse(), sg.B("Clear", key='Clear_Exe_Ahk', enable_events=True)],
                       [sg.T('Autohotkey Scripts Dir:'), sg.I(Status.getAhkUserDir(), key='Set_ScriptsDir_Ahk', enable_events=True), sg.FolderBrowse(), sg.B("Clear", key='Clear_ScriptsDir_Ahk', enable_events=True)]]
 
 #### Main UI Layout ####
 layout = [[sg.T('Environment:', font='bold'), sg.T(f'Windows OS: {osVersion.major}, Build: {osVersion.build}'), sg.T(f'Python: {pyVersion}'), sg.T(f'Dragon Version: {Status.getDNSVersion()}')],
           #### Projects Checkbox ####
+          [sg.T('Natlink Loglevel:'),  sg.Combo(default_value=Status.getLogging().title(), values=("Critical",  "Fatal",  "Error", "Warning", "Info" , "Debug"), key='Set_Logging_Natlink', enable_events=True, auto_size_text=True, readonly=True)],
           [sg.T('Configure Projects:', font='bold')],
-          [sg.Checkbox('Natlink', enable_events=True, key='natlink-checkbox'), sg.Checkbox('Dragonfly', enable_events=True, key='dragonfly-checkbox'), sg.Checkbox('Vocola', enable_events=True, key='vocola2-checkbox'), sg.Checkbox('Unimacro', enable_events=True, key='unimacro-checkbox'), sg.Checkbox('AutoHotkey', key='autohotkey-checkbox', enable_events=True)],
+          [sg.Checkbox('Dragonfly', enable_events=True, key='dragonfly-checkbox'), sg.Checkbox('Vocola', enable_events=True, key='vocola2-checkbox'), sg.Checkbox('Unimacro', enable_events=True, key='unimacro-checkbox'), sg.Checkbox('AutoHotkey', key='autohotkey-checkbox', enable_events=True)],
           #### Projects Hidden  See Hidden UI Columns above ####
-          [collapse(natlink_section, 'natlink', natlink)],
           [collapse(dragonfly_section, 'dragonfly', dragonfly)],
           [collapse(vocola2_section, 'vocola2', vocola)],
           [collapse(unimacro_section, 'unimacro', unimacro)],
@@ -72,14 +67,6 @@ window = sg.Window('Natlink GUI', layout)
 # Natlink
 def SetNatlinkLoggingOutput(values, event):
     Config.setLogging(values['Set_Logging_Natlink'])
-
-def NatlinkUserDir(values, event):
-    if event.startswith('Set'):
-        # Get the value from the input field with key 'Set_UserDir_Natlink' and set it as the UserDirectory
-        Config.setDirectory('UserDirectory', values['Set_UserDir_Natlink']) 
-    if event.startswith('Clear'):
-        Config.clearDirectory('UserDirectory') # NatlinkCore Function to clear the directory
-        window['Set_UserDir_Natlink'].update("") # Clear the input box with 'Set_UserDir_Natlink' key in GUI
 
 # Dragonfly
 def DragonflyUserDir(values, event):
@@ -138,7 +125,7 @@ def AhkUserDir(values, event):
 
 
 # Lookup dictionary that maps keys as events to a function to call in Event Loop.
-nalink_dispatch = {'Set_Logging_Natlink': SetNatlinkLoggingOutput, 'Set_UserDir_Natlink': NatlinkUserDir, 'Clear_UserDir_Natlink': NatlinkUserDir}
+nalink_dispatch = {'Set_Logging_Natlink': SetNatlinkLoggingOutput}
 dragonfly_dispatch = {'Set_UserDir_Dragonfly': DragonflyUserDir, 'Clear_UserDir_Dragonfly': DragonflyUserDir}
 vocola_dispatch = {'Set_UserDir_Vocola': VocolaUserDir, 'Clear_UserDir_Vocola': VocolaUserDir,'Vocola_Lang': VocolaTakesLanguages,'Vocola_Unimacro_Actions': VocolaUnimacroActions}  # , 'Set_GrammarsDir_Vocola': VocolaGrammarsDir
 unimacro_dispatch = {'Set_UserDir_Unimacro': UnimacroUserDir, 'Clear_UserDir_Unimacro': UnimacroUserDir}
@@ -153,12 +140,7 @@ try:
             break
         # Hidden Columns logic
         # TODO: if project is enabled, then show the column
-        elif event.startswith('natlink'):
-            natlink = not natlink
-            window['natlink-checkbox'].update(natlink)
-            window['natlink'].update(visible=natlink)
-
-        if event.startswith('dragonfly'):
+        elif event.startswith('dragonfly'):
             dragonfly = not dragonfly
             window['dragonfly-checkbox'].update(dragonfly)
             window['dragonfly'].update(visible=dragonfly)
@@ -180,11 +162,11 @@ try:
 
         # Dispatch events to call appropriate config function.
         if event in nalink_dispatch:
-            func_to_call = nalink_dispatch[event] # get function from dispatch dictionary (nalink_dispatch)
-            func_to_call(values, event) # event is passed to function for event specific handling. Set\Clear
-        if event in dragonfly_dispatch:
-            func_to_call = dragonfly_dispatch[event]
+            func_to_call = nalink_dispatch[event]
             func_to_call(values, event)
+        if event in dragonfly_dispatch:
+            func_to_call = dragonfly_dispatch[event] # get function from dispatch dictionary (dragonfly_dispatch)
+            func_to_call(values, event) # event is passed to function for event specific handling. Set\Clear
         if event in vocola_dispatch:
             func_to_call = vocola_dispatch[event]
             func_to_call(values, event)
