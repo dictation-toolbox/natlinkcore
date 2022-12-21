5b7c700001c78360d81dbb10044400e443432e2a
Doug Ransom <doug.ransom@gmail.com>
2022-12-21 10:10:22 -0800

    checkpoint

---
+++ src/natlinkcore/configure/natlink_extensions.py
@@ -0,0 +1,26 @@
+"""Command Line Program to List Advertised Natlink Exensions."""
+import sys
+from importlib.metadata import entry_points
+import argparse
+
+def main():
+    parser=argparse.ArgumentParser(description="Enumerate natlink extension momdules.")
+    args=parser.parse_args()
+
+    discovered_extensions=entry_points(group='natlink_extensions')
+
+    for extension in discovered_extensions:
+        n=extension.name
+        ep=extension.value
+        try:
+            pathfn=extension.load()
+            path=pathfn()
+
+        except Exception as e:
+            path = e   
+
+        print(f"\n{n} {path}")
+        return 0
+ 
+if '__main__' == __name__:
+    main()
