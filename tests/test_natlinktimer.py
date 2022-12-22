fdc97aeffc65b98df57f7951ae226211862c36c3
Quintijn Hoogenboom <q.hoogenboom@antenna.nl>
2022-12-22 16:15:34 +0100

    check lines in the cancelMode routine of test grammar of natlinktimer.

--- tests/test_natlinktimer.py
+++ tests/test_natlinktimer.py
@@ -44,9 +44,15 @@ class TestGrammar(GrammarBase):
         self.sleepTime = 0 # to be specified by calling instance, the sleeping time after each hit
         self.results = []
         self.starttime = round(time.time()*1000)
+        self.startCancelMode = False
+        self.endCancelMode = False
 
     def cancelMode(self):
+        """timer should stop, and the 2 instance variables set to True
+        """
+        self.startCancelMode = True
         natlinktimer.setTimerCallback(self.doTimerClassic,0)
+        self.endCancelMode = True
 
     def doTimerClassic(self):
         """have no introspection, but be as close as possible to the old calling method of setTimerCallback
@@ -102,7 +108,58 @@ class TestGrammar(GrammarBase):
     
 TestGrammar.__test__ = False
 
-    
+    def testIcons(self):
+        """in the subdirectory icons of Unimacro there are 4 icons
+        
+        These should show up when natlink.setTrayIcon('_repeat.ico', 'tooltip', func) is called,
+        but this is not working any more.
+        There are also predefined icons, which are now stored in defaulticons directory of unimacro (fork)
+        
+        I don't know how to call these again...
+        
+It was tested in C:\DT\NatlinkDoug\pythonsrc\tests\unittestNatlink.py, this test could be taken out and put into
+a pytest module!
+        
+**From natlink.txt**:
+        
+setTrayIcon( iconName, toolTip, callback )
+	This function, provided by Jonathan Epstein, will draw an icon in the
+	tray section of the tackbar.  
+
+	Pass in the absolute path to a Windows icon file (.ico) or pass in one
+	of the following predefined names: 
+		'right', 'right2', 'down', 'down2',
+		'left', 'left2', 'up', 'up2', 'nodir' 
+	You can also pass in an empty string (or nothing) to remove the tray
+	icon.
+
+	The toolTip parameter is optional.  It is the text which is displayed
+	as a tooltip when the mouse is over the tray icon.  If missing, a generic
+	tooltip is used.
+
+	The callback parameter is optional.  When used, it should be a Python
+	function which will be called when a mouse event occurs for the tray
+	icon.  The function should take one parameters which is the type of
+	mouse event:
+		wm_lbuttondown, wm_lbuttonup, wm_lbuttondblclk, wm_rbuttondown, 
+		wm_rbuttonup, wm_rbuttondblclk, wm_mbuttondown, wm_mbuttonup, 
+		or wm_mbuttondblclk (all defined in natlinkutils)
+
+	Raises ValueError if the iconName is invalid.
+
+The following functions are used in the natlinkmain base module.  You
+should only used these if you are control NatSpeak using the NatLink module
+instead of using Python as a command and control subsystem for NatSpeak. In
+the later case, users programs should probably not use either of these two
+functions because they replace the callback used by the natlinkmain module
+which could prevent proper module (re)loading and user changes.
+        
+        """
+        natlink.setTrayIcon('_repeat.ico')
+        natlink.setTrayIcon('_down')   # ????
+        
+        
+        
 # def testSingleTimerClassic():
 #     try:
 #         natlink.natConnect()
@@ -161,6 +218,8 @@ def testStopAtMicOff():
         print(f'testGram.results: {testGram.results}')
         assert len(testGram.results) == testGram.toggleMicAt
         assert natlinktimer.getNatlinktimerStatus() == 0    ## natlinktimer is NOT destroyed after last timer is gone. 
+        assert testGram.startCancelMode is True
+        assert testGram.endCancelMode is True
 
     finally:
         natlinktimer.stopTimerCallback()
