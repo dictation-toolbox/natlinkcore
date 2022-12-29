e3e008f6c58cfc07c139cbfd3afa52321e8a1b85
Quintijn Hoogenboom <q.hoogenboom@antenna.nl>
2022-12-29 12:28:23 +0100

    Skip debug message (#42)
    * natlinktimer.py seems to work, including callback at mic off.
    * remove debug lines "skipping unchanged ..." from loader.py
    
    Co-authored-by: Doug Ransom <doug.ransom@gmail.com>

--- src/natlinkcore/loader.py
+++ src/natlinkcore/loader.py
@@ -314,7 +314,7 @@ class NatlinkMain(metaclass=Singleton):
                     self.loaded_modules[mod_path] = module
                     return
                 else:
-                    self.logger.info(f'skipping unchanged bad module: {mod_name}')
+                    # self.logger.debug(f'skipping unchanged bad module: {mod_name}')
                     return
             else:
                 maybe_module = self.loaded_modules.get(mod_path)
@@ -341,7 +341,7 @@ class NatlinkMain(metaclass=Singleton):
                         self.logger.debug(f'loaded module: {module.__name__}')
                         return
                     else:
-                        self.logger.debug(f'skipping unchanged loaded module: {mod_name}')
+                        # self.logger.debug(f'skipping unchanged loaded module: {mod_name}')
                         return
         except Exception:
             self.logger.exception(traceback.format_exc())
