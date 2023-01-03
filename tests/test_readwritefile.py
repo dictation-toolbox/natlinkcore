ef868ef42d45fa0ae27c28a287444f99c10c909e
Quintijn Hoogenboom <q.hoogenboom@antenna.nl>
2023-01-03 13:30:49 +0100

    Issue 48 (#49)
    * issue #48, cleanup code in __init__.py
    move testing setTrayIcon to natlink itself.
    
    Co-authored-by: Doug Ransom <doug.ransom@gmail.com>

--- tests/test_readwritefile.py
+++ tests/test_readwritefile.py
@@ -4,11 +4,14 @@ import os
 import configparser
 import pytest
 from natlinkcore.readwritefile import ReadWriteFile
+from pathlib import Path
 
 thisFile = __file__
 thisDir, Filename = os.path.split(thisFile)
 testDir = os.path.join(thisDir, 'readwritefiletest')
+testFolderName="readwritefiletest"
 
+mock_readwritefiledir=Path(thisDir)/"mock_readwritefile"
 
 def setup_module(module):
     pass
@@ -19,11 +22,16 @@ def teardown_module(module):
             F_path = os.path.join(testDir, F)
             os.remove(F_path)
 
-def test_only_write_file():
-    join, isfile = os.path.join, os.path.isfile
-    newFile = join(testDir, 'output-newfile.txt')
-    if isfile(newFile):
-        os.unlink(newFile)
+def test_only_write_file(tmp_path):
+    print(f"Temp path: {tmp_path}")
+    testDir = tmp_path / testFolderName
+    testDir.mkdir()
+
+ #   join, isfile = os.path.join, os.path.isfile
+ #   newFile = join(testDir, 'output-newfile.txt')
+ #   if isfile(newFile):
+ #       os.unlink(newFile)
+    newFile= testDir/'output-newfile.txt'
     rwfile = ReadWriteFile()
     text = ''
     rwfile.writeAnything(newFile, text)
@@ -36,11 +44,14 @@ def test_only_write_file():
     assert rwfile.bom == ''
     assert text == ''
     
-def test_accented_characters_write_file():
-    join, isfile = os.path.join, os.path.isfile
-    newFile = join(testDir, 'output-accented.txt')
-    if isfile(newFile):
-        os.unlink(newFile)
+def test_accented_characters_write_file(tmp_path):
+#    join, isfile = os.path.join, os.path.isfile
+#    testDir = tmp_path / testFolderName
+#    testDir.mkdir()
+ #   newFile = join(testDir, 'output-accented.txt')
+    testDir = tmp_path / testFolderName
+    testDir.mkdir()
+    newFile = testDir/"outut-accented.txt"
     text = 'caf\xe9'
     rwfile = ReadWriteFile(encodings=['ascii'])  # optional encoding
     # this is with default errors='xmlcharrefreplace':
@@ -72,9 +83,13 @@ def test_accented_characters_write_file():
     text_back = rwfile_utf.readAnything(newFile)
     assert text == text_back
 
-def test_other_encodings_write_file():
-    join = os.path.join
-    oldFile = join(testDir, 'latin1 accented.txt')
+def test_other_encodings_write_file(tmp_path):
+     
+    testDir = tmp_path / testFolderName
+    testDir.mkdir()
+
+    oldFile = mock_readwritefiledir/'latin1 accented.txt'
+
     rwfile = ReadWriteFile(encodings=['latin1'])  # optional encoding
     text = rwfile.readAnything(oldFile)
     assert text == 'latin1 café'
@@ -83,56 +98,66 @@ def test_other_encodings_write_file():
     
 
 
-def test_latin1_cp1252_write_file():
-    join = os.path.join
-    _newFile = join(testDir, 'latin1.txt')
-    _newFile = join(testDir, 'cp1252.txt')
+def test_latin1_cp1252_write_file(tmp_path):
+    testDir = tmp_path / testFolderName
+    testDir.mkdir()
+    _newFile = testDir/ 'latin1.txt'
+    _newFile = testDir/'cp1252.txt'
+    assert False, "QH TODO"
+
     # TODO (QH) to be done, these encodings do not take all characters,
     # and need special attention.
     # (as long as the "fallback" is utf-8, all write files should go well!)
 
-def test_read_write_file():
+def test_read_write_file(tmp_path):
     listdir, join, splitext = os.listdir, os.path.join, os.path.splitext
-    for F in listdir(testDir):
+    testDir = tmp_path / testFolderName
+    testDir.mkdir()
+    mock_files_list=listdir(mock_readwritefiledir)
+    assert len(mock_files_list) > 0
+
+    for F in mock_files_list:
         if not F.startswith('output-'):
             Fout = 'output-' + F
-            F_path = join(testDir, F)
+            #read the file from the mock folder
+            F_path =   mock_readwritefiledir / F
             rwfile = ReadWriteFile()
             text = rwfile.readAnything(F_path)
             trunk, _ext = splitext(F)
             Fout = trunk + ".txt"
-            Fout_path = join(testDir, Fout)
+            Fout_path = testDir/ Fout
+            #write to our temp folder
             rwfile.writeAnything(Fout_path, text)
+            #make sure they are the same
             assert open(F_path, 'rb').read() == open(Fout_path, 'rb').read()
-            
-def test_read_config_file():
-    listdir, join, splitext = os.listdir, os.path.join, os.path.splitext
-    for F in listdir(testDir):
-        if F.endswith('.ini'):
-            if F == 'acoustics.ini':
-                F_path = join(testDir, F)
-                rwfile = ReadWriteFile()
-                config_text = rwfile.readAnything(F_path)
-                Config = configparser.ConfigParser()
-                Config.read_string(config_text)
-                assert Config.get('Acoustics', '2 2') == '2_2' 
-                continue
 
-            if F in ['natlink.ini', 'natlinkconfigured.ini']:
-                F_path = join(testDir, F)
-                rwfile = ReadWriteFile()
-                config_text = rwfile.readAnything(F_path)
-                Config = configparser.ConfigParser()
-                Config.read_string(config_text)
-                debug_level = Config.get('settings', 'log_level')
-                assert debug_level == 'DEBUG'
-                Config.set('settings', 'log_level', 'INFO')
-                new_debug_level = Config.get('settings', 'log_level')
-                assert new_debug_level == 'INFO'
-                trunk, ext = splitext(F)
-                Fout = trunk + 'out' + ext
-                Fout_path = join(testDir, Fout)
-                Config.write(open(Fout_path, 'w', encoding=rwfile.encoding))
+def test_acoustics_ini(tmp_path):
+    F='acoustic.ini'
+    F_path = mock_readwritefiledir/F
+    rwfile = ReadWriteFile()
+    config_text = rwfile.readAnything(F_path)
+    Config = configparser.ConfigParser()
+    Config.read_string(config_text)
+    assert Config.get('Acoustics', '2 2') == '2_2' 
+
+@pytest.mark.parametrize("F", ['originalnatlink.ini', 'natlinkconfigured.ini'])
+def test_config_ini(tmp_path,F):
+    F_path = mock_readwritefiledir/ F
+    testDir = tmp_path / testFolderName
+    testDir.mkdir()
+    rwfile = ReadWriteFile()
+    config_text = rwfile.readAnything(F_path)
+    Config = configparser.ConfigParser()
+    Config.read_string(config_text)
+    debug_level = Config.get('settings', 'log_level')
+    assert debug_level == 'DEBUG'
+    Config.set('settings', 'log_level', 'INFO')
+    new_debug_level = Config.get('settings', 'log_level')
+    assert new_debug_level == 'INFO'
+    Fout_path = testDir/F
+    Config.write(open(Fout_path, 'w', encoding=rwfile.encoding))
+
+
 
 if __name__ == "__main__":
     pytest.main(['test_readwritefile.py'])
