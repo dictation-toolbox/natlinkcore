3cfc5ce223e17724263f4c513754e4ec48a5acfe
Quintijn Hoogenboom <q.hoogenboom@antenna.nl>
2024-02-04 16:42:03 +0100

    the buttonclicktest.py shows the bug when natlink.playEvents is called.

--- tests/buttonclicktest.py
+++ tests/buttonclicktest.py
@@ -1,14 +1,27 @@
 ### buttonClick seems to cause an "ESP" runtime error with Dragon 16.
+# via unimacroutils it runs via natlink.execScript
+
+# via natlinkutils, the code runs (the right click is performed), but afterwards
+# the "ESP" error is hit.
+
+# When Dragon is running, it freezes, and must be closed with the windows task manager
+
 
 import natlink
+from natlinkcore import natlinkutils
 from dtactions.unimacro import unimacroutils
 
 if __name__ == "__main__":
     try:
         natlink.natConnect()
         print('try a buttonclick')
-        natlinkutils.buttonClick('left', 1)
+        unimacroutils.buttonClick('left', 2)
         print('after the buttonClick')
+        
+        print('now via natlinkutils.buttonClick (right click)')
+        print('the code runs, but the "ESP" error window appears.')
+        natlinkutils.buttonClick('right', 1)
+        print('after the natlinkutils.buttonClick')
     finally:
         natlink.natDisconnect()
         
