1f8f5986931e7d738d5006748a92e6c27e822043
Quintijn Hoogenboom <q.hoogenboom@antenna.nl>
2024-05-02 12:02:15 +0200

    Dr16efforts, changing natlinkcore.toml (#76)
    * bump to (release) 5.3.12
    
    * remove natlink from the dependencies of natlinkcore... update dtactions dependency
    
    ---------
    
    Co-authored-by: Doug Ransom <doug.ransom@gmail.com>

--- src/natlinkcore/__init__.py
+++ src/natlinkcore/__init__.py
@@ -1,6 +1,6 @@
 '''Python portion of Natlink, a compatibility module for Dragon Naturally Speaking
 The python stuff including test modules'''
-__version__="5.3.11"
+__version__="5.3.12"
 #pylint:disable=
 from pathlib import Path
 
